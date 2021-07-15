c10f69c760d90c3ff5801d0c7a0bc7fb239cbc90
Chris Galbraith <chris@resknow.co.uk>
2021-07-15 10:00:01 +0100

    Remove dir constant

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ use PHPty\PHPty;
 use PHPty\LocateFiles;
 use PHPty\Builder;
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/src/helpers/compat.php';
+require_once 'vendor/autoload.php';
+require_once 'src/helpers/compat.php';
 
 $builder = new Builder(new PHPty);
 $builder->build();
