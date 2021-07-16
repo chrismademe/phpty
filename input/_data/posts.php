5dcb12d4a481fd852a87bbb180771cc33e6191cd
Chris Galbraith <chris@resknow.co.uk>
2021-07-16 16:15:30 +0100

    Use example API for demo collection

--- input/_data/posts.php
+++ input/_data/posts.php
@@ -1,16 +1,19 @@
 <?php
 
-return [
-    [
-        'slug' => 'hello-world',
-        'title' => 'Hello World',
-        'content' => '<p>Post content and such</p>',
-        'date' => date('Y-m-d H:i:s')
-    ],
-    [
-        'slug' => 'hello-world-ii',
-        'title' => 'Hello World II',
-        'content' => '<p>Post such and such</p>',
-        'date' => date('Y-m-d H:i:s')
-    ]
-];
+$posts = file_get_contents( 'https://jsonplaceholder.typicode.com/posts' );
+return json_decode($posts, true);
+
+// return [
+//     [
+//         'slug' => 'hello-world',
+//         'title' => 'Hello World',
+//         'content' => '<p>Post content and such</p>',
+//         'date' => date('Y-m-d H:i:s')
+//     ],
+//     [
+//         'slug' => 'hello-world-ii',
+//         'title' => 'Hello World II',
+//         'content' => '<p>Post such and such</p>',
+//         'date' => date('Y-m-d H:i:s')
+//     ]
+// ];
